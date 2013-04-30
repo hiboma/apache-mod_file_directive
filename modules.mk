fde896363381883254221a9acdc9797985dc3523
id:hiboma(ひろや) <hiroyan@gmail.com>
2013-04-30 19:08:13 +0900

    removed gomi

--- modules.mk
+++
@@ -1,4 +0,0 @@
-mod_file_directive.la: mod_file_directive.slo
-	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version  mod_file_directive.lo
-DISTCLEAN_TARGETS = modules.mk
-shared =  mod_file_directive.la
